5e9f775137acf28e6b42439d5b1553f9dedc18cd
Suzy Hwang <soyeongsuzyhwang@gmail.com>
2021-12-06 01:06:59 +0900

    [MainPage][Header, Footer] header, footer 구현

---
+++ public/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDhjIW_K5mnfZhlyAMFJUi8qDvt7RwofZQ",
+  authDomain: "decheck-c8343.firebaseapp.com",
+  projectId: "decheck-c8343",
+  storageBucket: "decheck-c8343.appspot.com",
+  messagingSenderId: "590656093406",
+  appId: "1:590656093406:web:b4a7f8be91a5ca4bfc164f",
+  measurementId: "G-8P4YGTX02W",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
